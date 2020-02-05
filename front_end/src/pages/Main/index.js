417dc3a1697fc580a84af9bb2030e55180e65e34
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2020-02-05 11:35:30 -0300

    Setting up enviroment and install dependencies

---
+++ front_end/src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { MainPage } from './styles';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <MainPage>
+          <h1>Autenticação de Usuário</h1>
+        </MainPage>
+      </>
+    );
+  }
+}
---
+++ front_end/src/pages/Main/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const MainPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 30px;
+
+  h1 {
+    margin-bottom: 30px;
+  }
+`;
+
+export const Auth = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  border: solid black;
+  border-radius: 10px;
+
+  padding: 10px 0 10px 0;
+
+  div {
+    margin-top: 2px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    margin: 20px 0 10px 0;
+  }
+`;
